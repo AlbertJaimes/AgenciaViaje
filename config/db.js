6ffc791e3f900b90302cabfdec57d99357a2889a
AlbertJaimes <alberjaimes2@hotmail.com>
2025-05-18 12:38:55 -0500

    actualizacion db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import Sequelize  from "sequelize";
 
-const db = new Sequelize('agenciaviajes', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize('baa6v3gkb4oakxfp58k5', 'u4udhvjqog9qhqk7', 'koHufySeqWx5nC15OGtl', {
+    host: 'baa6v3gkb4oakxfp58k5-mysql.services.clever-cloud.com',
     dialect: '3306',
     dialect: 'mysql',
     define: {
